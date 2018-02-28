c152c261a82876dcc89ab612bb26d7375ad14e40
Joffre Elise <elise.joffre@etu.unilim.fr>
2018-02-28 09:58:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/elisejoffre/cycledevie/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.elisejoffre.cycledevie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by elisejoffre on 28/02/2018.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
